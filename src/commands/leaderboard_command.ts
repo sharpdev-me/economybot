401b4fb94fd9058c46744a70dc978e744534dc43
sharpdev-me <digidev.devx@gmail.com>
2020-09-11 13:37:31 -0700

    Leaderboard Command

---
+++ src/commands/leaderboard_command.ts
@@ -0,0 +1,43 @@
+import { Message, MessageEmbed } from "discord.js";
+import { GuildSettings, getBalances, getBalance } from "../database";
+
+export async function run(args: string[], message: Message, settings?: GuildSettings) {
+    if(!settings) {
+        return message.channel.send("This command can only be run in a server!").catch(console.error);
+    }
+    const balances = await getBalances(message.guild.id);
+
+    let amount: number = 10;
+
+    if(args.length > 0) {
+        amount = Number(args[0]);
+        if(isNaN(amount)) {
+            return message.channel.send("Amount must be a number!").catch(console.error);
+        }
+    }
+
+    const yourPlace = balances.findIndex((value) => {
+        return value.userID == message.author.id;
+    });
+
+    const embed = new MessageEmbed()
+        .setColor(0xFFF700)
+        .setFooter(`Your position: ${yourPlace + 1}`)
+        .setTitle(`Top ${amount} Richest Players`);
+
+    let value = "";
+
+    if(amount >= balances.length) amount = balances.length;
+
+    for(let i = 0; i < amount; i++) {
+        const element = balances[i];
+        const user = await message.client.users.fetch(element.userID);
+        value += `${i + 1}. **${user.username}#${user.discriminator}**: ${element.balance} ${settings.currency}\n`;
+    }
+
+    embed.setDescription(value);
+    message.channel.send(embed).catch(console.error);
+}
+
+export const name = "leaderboard";
+export const aliases = ["leader", "top", "lb"];
