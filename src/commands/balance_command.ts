ff32e8408eb0ee68cc83319bbf81bf54ffa58de4
sharpdev-me <digidev.devx@gmail.com>
2020-09-11 20:40:27 -0700

    Change setbalance to use mentions instead of IDs

--- src/commands/balance_command.ts
+++ src/commands/balance_command.ts
@@ -2,6 +2,9 @@ import { Message } from "discord.js";
 import { GuildSettings, getBalance } from "../database";
 
 export async function run(args: string[], message: Message, settings?: GuildSettings) {
+    if(!settings) {
+        return message.channel.send("This command can only be run in a server!").catch(console.error);
+    }
     const balance = await getBalance(message.author.id, message.guild.id);
     message.channel.send(`Your balance is \`${balance.balance} ${settings.currency}\``).catch(console.error);
 }
--- src/commands/set_balance_command.ts
+++ src/commands/set_balance_command.ts
@@ -2,18 +2,21 @@ import { Message } from "discord.js";
 import { GuildSettings, getBalance } from "../database";
 
 export async function run(args: string[], message: Message, settings?: GuildSettings) {
+    if(!settings) {
+        return message.channel.send("This command can only be run in a server!").catch(console.error);
+    }
+    if(settings.managers.findIndex((u) => {return u === message.author.id}) === -1 && message.author.id !== message.guild.ownerID) {
+        return message.channel.send("You do not have permission to execute this command!").catch(console.error);
+    }
     if(args.length < 1) {
-        return message.channel.send("Proper usage is \`set_balance <balance> [user1, user2...]`").catch(err => {
-            console.error(err);
-            message.channel.send("It seems there was an error processing your command! Please report this issue as soon as you can.").catch(console.error);
-        });
+        return message.channel.send("Proper usage is \`set_balance <balance> [user1, user2...]`").catch(console.error);
     }
     const balance: number = Number(args[0]);
     if(isNaN(balance)) {
         return message.channel.send("Balance must be a number!").catch(console.error);
     }
     
-    if(args.length == 1) {
+    if(message.mentions.members.size < 1) {
         const userBalance = await getBalance(message.author.id, message.guild.id);
         userBalance.balance = balance;
         userBalance.save().then(() => {
@@ -24,19 +27,21 @@ export async function run(args: string[], message: Message, settings?: GuildSett
         });
         return;
     }
-    args.shift();
-    for (const id of args) {
-        const userBalance = await getBalance(id, message.guild.id);
+    let error = false;
+    message.mentions.members.forEach(async member => {
+        const userBalance = await getBalance(member.id, message.guild.id);
         userBalance.balance = balance;
-        userBalance.save().then(() => {
-            message.channel.send("Those balances have been updated!").catch(console.error);
-        }).catch(err => {
+        userBalance.save().catch(err => {
             console.error(err);
-            message.channel.send("There was an issue saving those balances! Please report this issue as soon as you can.").catch(console.error);
+            error = true;
         });
         return;
+    })
+    if(error) {
+        return message.channel.send("There was an issue saving those balances! Please report this issue as soon as you can.").catch(console.error);
     }
+    message.channel.send("Those balance(s) have been updated!").catch(console.error);
 }
 
 export const name = "set_balance";
-export const aliases = ["setbalances","sb"];
+export const aliases = ["setbalances","sb", "set_balances", "setbalance"];
