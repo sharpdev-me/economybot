edc8f22e33510b3179ce26ce36fbbbfe8e26f2ba
sharpdev-me <68045886+sharpdev-me@users.noreply.github.com>
2020-11-25 19:19:23 -0800

    Display all settings when run with no arguments

--- src/commands/settings_command.ts
+++ src/commands/settings_command.ts
@@ -1,4 +1,4 @@
-import { Message } from "discord.js";
+import { Message, MessageEmbed } from "discord.js";
 import { getEventSettings, GuildSettings } from "../database";
 
 export async function run(args: string[], message: Message, settings?: GuildSettings) {
@@ -9,116 +9,112 @@ export async function run(args: string[], message: Message, settings?: GuildSett
     if(settings.managers.findIndex((u) => {return u === message.author.id}) === -1 && message.author.id !== message.guild.ownerID) {
         return message.channel.send("You do not have permission to execute this command!").catch(console.error);
     }
+    const eventSettings = await getEventSettings(settings.id);
     if(args.length < 1) {
-        return message.channel.send("Proper usage is \`settings <setting> [new value]\`").catch(console.error);
+        const embed = new MessageEmbed()
+            .setColor(0xFFF700)
+            .setFooter("Run `settings <setting> <value>` to change a value.")
+            .setTitle("Server Settings")
+            .setThumbnail(message.guild.iconURL());
+        let value = "";
+
+        value += `\`prefix\`: ${settings.prefix} - The symbol placed before commands\n`;
+        value += `\`defaultBalance\`: ${settings.defaultBalance} - The balance to give new members\n`;
+        value += `\`currency\`: ${settings.currency} - The name of your currency\n`;
+        value += `\`watchMessages\`: ${eventSettings.watchMessages} - Enable rewards for sending messages\n`;
+        value += `\`messageReward\`: ${eventSettings.messageReward} - The amount to reward a message\n`;
+        value += `\`messageCooldown\`: ${eventSettings.messageCooldown} - The time in milliseconds between messages to get a reward\n`;
+        value += `\`referrals\`: ${eventSettings.referrals} - Enable getting rewards for referrals\n`;
+        value += `\`referrerAmount\`: ${eventSettings.referrerAmount} - The amount to give the referrer when their link is used\n`;
+        value += `\`referredAmount\`: ${eventSettings.referredAmount} - The amount to give the user who was referred by a link\n`;
+        
+
+        embed.setDescription(value);
+
+        return message.channel.send(embed).catch(console.error);
+    }
+    if(args.length < 2) {
+        return message.channel.send("Proper usage is `settings [<setting> <value>]`").catch(console.error);
     }
     const setting = args[0];
-    const eventSettings = await getEventSettings(settings.id);
-    if(args.length == 1) {
-        let result = "";
-        switch(setting) {
-            case "prefix":
-                result = settings.prefix;
-                break;
-            case "defaultBalance":
-                result = "" + settings.defaultBalance;
-                break;
-            case "currency":
-                result = settings.currency;
-                break;
-            case "watchMessages":
-                result = eventSettings.watchMessages ? "true" : "false";
-                break;
-            case "messageReward":
-                result = eventSettings.messageReward + "";
-                break;
-            case "messageCooldown":
-                result = eventSettings.messageCooldown + "";
-                break;
-            case "watchInvites":
-                result = eventSettings.watchInvites ? "true" : "false";
-                break;
-            case "inviteReward":
-                result = eventSettings.inviteReward + "";
-                break;
-            default:
-                message.channel.send("That setting does not exist. Available settings are `prefix, defaultBalance, currency, watchMessages, messageReward, messageCooldown, watchInvites, inviteReward`").catch(console.error);
-                return;
-        }
-        message.channel.send(`The value for \`${setting}\` is \`${result}\``).catch(console.error);
-    } else if(args.length > 1) {
-        switch(setting) {
-            case "prefix":
-                settings.prefix = args[1];
-                break;
-            case "defaultBalance":
-                let n = Number(args[1]);
-                if(isNaN(n)) {
-                    return message.channel.send("defaultBalance must be a number!").catch(console.error);
-                }
-                settings.defaultBalance = n;
-                break;
-            case "currency":
-                settings.currency = args[1];
-                break;
-            case "watchMessages":
-                let s = args[1];
-                let v: boolean;
-                if(s === "true") {
-                    v = true;
-                } else if(s === "false") {
-                    v = false;
-                } else {
-                    return message.channel.send("watchMessages must be either `true` or `false`").catch(console.error);
-                }
-                eventSettings.watchMessages = v;
-                break;
-            case "messageReward":
-                let n1 = Number(args[1]);
-                if(isNaN(n1)) {
-                    return message.channel.send("messageReward must be a number!").catch(console.error);
-                }
-                eventSettings.messageReward = n1;
-                break;
-            case "messageCooldown":
-                let n2 = Number(args[1]);
-                if(isNaN(n2)) {
-                    return message.channel.send("messageCooldown must be a number!").catch(console.error);
-                }
-                eventSettings.messageCooldown = n2;
-                break;
-            case "watchInvites":
-                let s1 = args[1];
-                let v1: boolean;
-                if(s1 === "true") {
-                    v1 = true;
-                } else if(s1 === "false") {
-                    v1 = false;
-                } else {
-                    return message.channel.send("watchInvites must be either `true` or `false`").catch(console.error);
-                }
-                eventSettings.watchInvites = v1;
-                break;
-            case "inviteReward":
-                let n3 = Number(args[1]);
-                if(isNaN(n3)) {
-                    return message.channel.send("inviteReward must be a number!").catch(console.error);
-                }
-                eventSettings.inviteReward = n3;
-                break;
-            default:
-                message.channel.send("That setting does not exist. Available settings are `prefix, defaultBalance, currency, watchMessages, messageReward, messageCooldown, watchInvites, inviteReward`").catch(console.error);
-                return;
+    switch(setting) {
+        case "prefix":
+            settings.prefix = args[1];
+            break;
+        case "defaultBalance":
+            let n = Number(args[1]);
+            if(isNaN(n) || n == Infinity) {
+                return message.channel.send("defaultBalance must be a number!").catch(console.error);
+            }
+            settings.defaultBalance = n;
+            break;
+        case "currency":
+            settings.currency = args[1];
+            break;
+        case "watchMessages":
+            let s = args[1];
+            let v: boolean;
+            if(s === "true") {
+                v = true;
+            } else if(s === "false") {
+                v = false;
+            } else {
+                return message.channel.send("watchMessages must be either `true` or `false`").catch(console.error);
+            }
+            eventSettings.watchMessages = v;
+            break;
+        case "messageReward":
+            let n1 = Number(args[1]);
+            if(isNaN(n1) || n1 == Infinity) {
+                return message.channel.send("messageReward must be a number!").catch(console.error);
+            }
+            eventSettings.messageReward = n1;
+            break;
+        case "messageCooldown":
+            let n2 = Number(args[1]);
+            if(isNaN(n2) || n2 == Infinity) {
+                return message.channel.send("messageCooldown must be a number!").catch(console.error);
+            }
+            eventSettings.messageCooldown = n2;
+            break;
+        case "referrals":
+            let s1 = args[1];
+            let v1: boolean;
+            if(s1 === "true") {
+                v1 = true;
+            } else if(s1 === "false") {
+                v1 = false;
+            } else {
+                return message.channel.send("referrals must be either `true` or `false`").catch(console.error);
+            }
+            eventSettings.referrals = v1;
+            break;
+        case "referrerAmount":
+            let n3 = Number(args[1]);
+            if(isNaN(n3) || n3 == Infinity) {
+                return message.channel.send("referrerAmount must be a number!").catch(console.error);
+            }
+            eventSettings.referrerAmount = n3;
+            break;
+        case "referredAmount":
+            let n4 = Number(args[1]);
+            if(isNaN(n4) || n4 == Infinity) {
+                return message.channel.send("referredAmount must be a number!").catch(console.error);
+            }
+            eventSettings.referredAmount = n4;
+            break;
+        default:
+            message.channel.send("That setting does not exist. Available settings are `prefix, defaultBalance, currency, watchMessages, messageReward, messageCooldown, referrals, referrerAmount, referredAmount`").catch(console.error);
+            return;
 
-        }
-        try {
-            await settings.save()
-            await eventSettings.save();
-            message.channel.send(`The value of \`${args[0]}\` has been set to \`${args[1]}\``).catch(console.error);
-        } catch(e) {
-            console.error(e);
-            message.channel.send("There was an error saving your settings. Please report this as soon as possible.").catch(console.error);
-        }
+    }
+    try {
+        await settings.save()
+        await eventSettings.save();
+        message.channel.send(`The value of \`${args[0]}\` has been set to \`${args[1]}\``).catch(console.error);
+    } catch(e) {
+        console.error(e);
+        message.channel.send("There was an error saving your settings. Please report this as soon as possible.").catch(console.error);
     }
 }
 
