d06a523053ede6c1d6a9eae88ae389f6632dc715
sharpdev-me <68045886+sharpdev-me@users.noreply.github.com>
2020-12-19 02:17:08 -0800

    Remove ms counter

--- src/commands/ping_command.ts
+++ src/commands/ping_command.ts
@@ -2,13 +2,7 @@ import { Message } from "discord.js";
 import { GuildSettings } from "../database";
 
 export async function run(args: string[], message: Message, settings?: GuildSettings) {
-    let current = new Date();
-    message.channel.send("Pong!").catch(console.error).then((message: Message) => {
-        let newDate = new Date();
-        let time = newDate.getMilliseconds() - current.getMilliseconds();
-        if(time <= 0) return;
-        message.edit("Pong! `" + time + "ms`").catch(console.error);
-    });
+    message.channel.send("Pong!").catch(console.error);
 }
 
 export const name = "ping";
