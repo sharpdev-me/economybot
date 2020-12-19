0a1d5f6eedb1a46ea8a3842e0f3b6a86a1008576
sharpdev-me <68045886+sharpdev-me@users.noreply.github.com>
2020-12-19 02:22:49 -0800

    Send about description in current channel

--- src/commands/about_command.ts
+++ src/commands/about_command.ts
@@ -2,13 +2,9 @@ import { Message } from "discord.js";
 import { GuildSettings } from "../database";
 
 export async function run(args: string[], message: Message, settings?: GuildSettings) {
-    message.author.createDM().then(dm => {
-        dm.send("EconomyBot was created by one person (SharpDev#1011) because he was tired of seeing different points systems and server economies.\n"
+    message.channel.send("EconomyBot was created by one person (SharpDev#1011) because he was tired of seeing different points systems and server economies.\n"
         + "If you just use these bots, you can help EconomyBot achieve its purpose by telling bot developers about me.\n"
-        + "If you're a bot developer, you can go to https://economybot.xyz/docs/ to learn how to include me in your projects!").catch(console.error);
-    }).catch(() => {
-        message.channel.send("I wasn't able to send you a direct message. Make sure you allow me to do so in your settings.").catch(console.error)
-    });
+        + "If you're a bot developer, you can go to https://economybot.xyz/docs/ to learn how to include me in your projects!").catch(console.error);    
 }
 
 export const name = "about";
