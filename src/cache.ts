b973189cfc8e8c814814b3e9bd61e61d8e9f0e93
sharpdev-me <68045886+sharpdev-me@users.noreply.github.com>
2020-12-20 23:24:59 -0800

    Add license information

--- src/cache.ts
+++ src/cache.ts
@@ -1,7 +1,19 @@
-// Cache GuildSettings until 30 seconds of inactivty
-// Cache EventSettings until 30 seconds of inactivity
-
-// https://www.npmjs.com/package/redis
+/* EconomyBot - A unifying economy API
+ * Copyright (C) 2020 Skyler Morgan <sharpdev@sharpdev.me>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 import * as redis from "redis";
 import { promisify } from "util";
